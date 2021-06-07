e11c093bd1a56b620dc8047012fad5c373caf663
Shrutika Lokhande <shrutikalokhande@gmail.com>
2021-06-07 08:00:42 +0530

    fetching data for all countries

--- src/utils/apiCallHandler.js
+++ src/utils/apiCallHandler.js
@@ -1,20 +1,22 @@
-let countries = [
-		'germany',
-		'united states of america',
-		'brazil',
-		'iceland',
-		'afghanistan',
-		'aland islands',
-		'albania',
-		'algeria',
-	];
+// let countries = [
+// 		'germany',
+// 		'united states of america',
+// 		'brazil',
+// 		'iceland',
+// 		'afghanistan',
+// 		'aland islands',
+// 		'albania',
+// 		'algeria',
+// 	];
 const getCountryDetails = async () => {
 	try {
-		let response = await Promise.all(
-			countries.map((country) =>
-				fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`).then((data) => data.json())
-			)
-		);
+		// let response = await Promise.all(
+		// 	countries.map((country) =>
+		// 		fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`).then((data) => data.json())
+		// 	)
+		// );
+		let response=await fetch(`https://restcountries.eu/rest/v2/all`).then((data) => data.json());
+
 		if (response) {
 			return response;
 		}
@@ -27,7 +29,7 @@ export const getAllData = async () => {
 	let performAPICall = await getCountryDetails();
 	let data=[];
 	for (let i = 0; i < performAPICall.length; i++) {
-		data.push(performAPICall[i][0]);
+		data.push(performAPICall[i]);
 	}
 	return data;
 };
